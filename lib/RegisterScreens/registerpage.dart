a1e4b9aca997b6659d39d60a714fd390b633f36a
abhay nishad <imabhaynishad@gmail.com>
2024-11-09 17:30:32 +0530

    adding firebase correctly

--- lib/RegisterScreens/registerpage.dart
+++ lib/RegisterScreens/registerpage.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:foodies/user_auth/firbase_auth_implementation/firebase_auth_service.dart';
+import 'package:foodies/HomeScreens/home_page.dart';
+
+import '../Utils/Utilities.dart';
 
 class Myregisterpage extends StatefulWidget {
   const Myregisterpage({super.key});
@@ -10,104 +13,156 @@ class Myregisterpage extends StatefulWidget {
 }
 
 class _MyregisterpageState extends State<Myregisterpage> {
-  final FirebaseAuthService _auth = FirebaseAuthService();
+  // bool _isRememberMeChecked = false;
+  bool loading = false;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
-  bool _isRememberMeChecked = false;
-
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
-  @override
+  FirebaseAuth _auth = FirebaseAuth.instance;
+@override
   void dispose() {
-    // TODO: implement dispose
 
-    _nameController.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: SingleChildScrollView(
-        child: Container(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 70),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                  child: Column(
+                children: [
+                  Image(image: AssetImage("assets/images/filed.png")),
+                  Text(
+                    "Register Page",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "Name",
+                        prefixIcon: Icon(Icons.sensor_occupied),
+                        border: OutlineInputBorder(
           
-          child: Padding(
-            padding: const EdgeInsets.only(left: 30,right: 30,top: 70),
-            child: Column(
-            
-              children: [
-                Image(image: AssetImage("assets/images/filed.png")),
-                Text("Register Page",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.deepOrange),),
-                SizedBox(height: 30,),
-            
-                TextField(
-                  controller: _nameController,
-            
-                  decoration: InputDecoration(
-                      labelText: "Name",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),),
-                SizedBox(height: 20,),
-            
-                TextField(
-            controller: _emailController,
-                  decoration: InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),),
-                SizedBox(height: 20,),
-            
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-            
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),),
-                SizedBox(height: 20,),
-                TextField(
-                  obscureText: true,
+                            borderRadius: BorderRadius.circular(10))),
+                  ),
+          
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _emailController,
+                    decoration:  InputDecoration(
+                        labelText: "Email",
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Enter your email";
+                      }else{
+                        return null;
+                      }
+                    },
+                  ),
+          
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+// Validation---------------------------------------------
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Enter your password";
+                      }else{
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "ConfirmPassword",
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "PhoneNumber",
+                        prefixIcon: Icon(Icons.phone),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        setState(() {
+                          loading = true;
+                        });
+                        _auth.createUserWithEmailAndPassword(
+                            email: _emailController.text.toString(),
+// Error handling .then se ---------------all code in utils folder
+                            password: _passwordController.text.toString()).then((value){
 
-                  decoration: InputDecoration(
-                      labelText: "ConfirmPassword",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),),
-                SizedBox(height: 20,),
-            
-                TextField(
-            
-                  decoration: InputDecoration(
-                      labelText: "PhoneNumber",
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),),
-                SizedBox(height: 20,),
-            
-            
-              ElevatedButton(onPressed: _register, child: Center(child: Text("Register",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white),)),style: ElevatedButton.styleFrom(backgroundColor: Colors.deepOrange),),
-            
-            
-              ],
-            ),
+                        }).onError((error, stackTrace){
+                          Utils().toastMessage(error.toString());
+                          setState(() {
+                            loading = false;
+                          });
+                        });
+
+                      }
+
+                    },
+                    child: Center(
+
+                        child: loading ?  CircularProgressIndicator(strokeWidth: 3, color: Colors.white,): Text(
+                      "Register",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepOrange),
+                  ),
+                ],
+              )),
+            ],
           ),
         ),
       ),
     );
-  }
-
-  void _register()async{
-    String name = _nameController.text;
-    String email = _emailController.text;
-    String password = _passwordController as String;
-
-    User? user = await _auth.signUpWithEmailandPassword(email, password);
-
-    if(user != null){
-
-      print("User is successfully created");
-      Navigator.pushNamed(context, "/home");
-    }else{
-      print("Some error happen");
-    }
-
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:foodies/splsh/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
